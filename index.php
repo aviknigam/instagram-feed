193781ecc452c7b35e7e976ad8a4bdd3cc322b47
Avik Nigam <business.aviknigam@gmail.com>
2017-11-30 16:49:31 +1100

    Basic layout

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+// https://scontent.cdninstagram.com/t51.2885-15/e35/23967411_502012043513677_2057498804633993216_n.jpg
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Instagram API</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" href="/assets/css/main.css">
+	</head>
+	<body>
+		<div class="container-fluid wrapper d-flex justify-content-center align-items-center">
+			<div class="input-group">
+				<span class="input-group-addon" id="basic-addon1">@</span>
+				<input type="text" class="form-control" placeholder="Username">
+				<span class="input-group-btn">
+					<button class="btn btn-secondary" type="button">Go!</button>
+				</span>
+			</div>
+		</div>
+
+		<!-- Main Pictures -->
+			<div class="container-fluid mt-5">
+				<div class="row">
+					<div class="col-sm-4 d-flex justify-content-center">
+						hi
+					</div>
+					<div class="col-sm-4 d-flex justify-content-center">
+						hi
+					</div>
+					<div class="col-sm-4 d-flex justify-content-center">
+						hi
+					</div>
+				</div>
+			</div>
+	</body>
+</html>
